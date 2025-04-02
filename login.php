29f8f69e7628bcb2ba5393dfcfdd5044970fc1d4
Dafrijder <nielsdegast@outlook.com>
2025-04-02 14:12:07 +0200

    header and login

--- login.php
+++ login.php
@@ -3,11 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once __DIR__.'/config/config.php'; ?>
+    <?php require_once __DIR__.'/resources/views/components/header.php'; ?>
     <title>login</title>
 </head>
 <body>
     <div class="wrapper">
-        
+        <div class="form-container">
+            <form action="./app/Http/Controllers/login-controler.php" method="post" id="login">
+                <input type="hidden" name="action" value="login">
+                <div class="form-group">
+                    <label for="username">username</label>
+                    <input type="text" name="username" id="username">
+                </div>
+                <div class="form-group">
+                    <label for="password">password</label>
+                    <input type="password" name="password" id="password">
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="login">
+                </div>
+            </form>
+            <form action="./app/Http/Controllers/login-controler.php" method="post" id="signup"></form>
+        </div>
     </div>
 </body>
 </html>
--- resources/views/components/header.php
+++ resources/views/components/header.php
@@ -1,10 +1,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php $base_ur ?>/css/style.css">
+    <link rel="stylesheet" href="/public_html/css/main.css">
+    <?php require_once __DIR__.'/../../../config/config.php'; ?>
 </head>
 <div class="wrapper">
     <header>
-
+        <nav>
+            <div class="navlinks">
+                <a href="<?php echo $base_url; ?>/index.php">Home</a>
+                <a href="<?php echo $base_url; ?>/public_html/views/games/games.php">Games</a>
+                <a href="<?php echo $base_url; ?>/public_html/views/games/create.php">Create</a>
+                <a href="<?php echo $base_url; ?>/login.php">login</a>
+            </div>
+        </nav>
     </header>
 </div>
