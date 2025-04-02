43941f9f0b55a909399354268fc6441d7751069f
Dafrijder <nielsdegast@outlook.com>
2025-04-02 13:56:29 +0200

    files

--- resources/views/components/header.php
+++ resources/views/components/header.php
@@ -1,1 +1,10 @@
-<?php require_once ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php $base_ur ?>/css/style.css">
+</head>
+<div class="wrapper">
+    <header>
+
+    </header>
+</div>
